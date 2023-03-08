dd582a4fc848ade1017c01494f6a621f33db1fc9
koovalin <koovalin@gmail.com>
2023-03-08 22:09:40 +0300

    Cleared Master

--- src/main/java/HomeWorks/homeWork_1.java
+++ src/main/java/HomeWorks/homeWork_1.java
@@ -1,23 +1,6 @@
 package HomeWorks;
 
 public class homeWork_1 {
-    public static void main(String[] args) {
-        int[] array = new int[]{552, 284, 552, 738, 839, 555};
-        minAndMaxInArr(array);
 
-    }
 
-    private static void minAndMaxInArr(int someArr[]) {
-        int max = someArr[0];
-        int min = someArr[0];
-        for (int i: someArr){
-            if (i < min){
-                min = i;
-            }
-            if (i > max){
-                max = i;
-            }
-        }
-        System.out.printf("Min = %s\nMax = %s", min,max);
-    }
 }
