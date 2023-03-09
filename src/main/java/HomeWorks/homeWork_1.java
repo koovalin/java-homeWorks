62f0e536fc76a261b00adbf7277a4915951f7dc9
koovalin <koovalin@gmail.com>
2023-03-09 22:17:22 +0300

    pull homeWork_1_task_3

--- src/main/java/HomeWorks/homeWork_1.java
+++ src/main/java/HomeWorks/homeWork_1.java
@@ -8,7 +8,6 @@ public class homeWork_1 {
         Scanner scan = new Scanner(System.in);
         System.out.println("Введи число: ");
         int numToSort = Integer.parseInt(scan.next());
-
         int[] myArray = new int[]{3, 4, 6, 7, 5, 2, 1, 5, 4, 3, 2, 1, 4, 5, 4};
         System.out.println("Array: " + Arrays.toString(myArray));
         System.out.println("Sorted array: " + Arrays.toString(arraySort(myArray, numToSort)));
